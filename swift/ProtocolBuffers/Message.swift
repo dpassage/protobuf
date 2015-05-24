959043447a2c44863b87df0888110864eb5f7c7d
David Paschich <dpassage@balveda.com>
2015-05-24 16:15:09 -0700

    Our first Swift file, only defining the protocol for all messages.

---
+++ swift/ProtocolBuffers/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  ProtocolBuffers
+//
+//  Created by David Paschich on 5/24/15.
+//  Copyright (c) 2015 David Paschich. All rights reserved.
+//
+
+import Foundation
+
+public protocol Message {
+
+}
