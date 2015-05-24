b6f4ce36760d811a9e1ede3e5eae079e863a0635
David Paschich <dpassage@balveda.com>
2015-05-24 16:09:47 -0700

    New empty framework project for ProtocolBuffers Swift runtime

---
+++ swift/ProtocolBuffers/ProtocolBuffers.h
@@ -0,0 +1,19 @@
+//
+//  ProtocolBuffers.h
+//  ProtocolBuffers
+//
+//  Created by David Paschich on 5/24/15.
+//  Copyright (c) 2015 David Paschich. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ProtocolBuffers.
+FOUNDATION_EXPORT double ProtocolBuffersVersionNumber;
+
+//! Project version string for ProtocolBuffers.
+FOUNDATION_EXPORT const unsigned char ProtocolBuffersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProtocolBuffers/PublicHeader.h>
+
+
